1d46e431c8b018ef9bdb756685bd04dc9ba12465
Andrezj <andre.oliveira@ccc.ufcg.edu.br>
2020-04-01 18:53:10 -0300

    Finalizando primmeira implementação do backend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -4,7 +4,22 @@ module.exports = {
     async index(request, response) {
         const { page = 1 } = request.query;
 
-        const incidents = await connection('incidents').select('*');
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select([
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
+
+        response.header('X-Toral-Count', count['count(*)']);
 
         return response.json(incidents);
     },
